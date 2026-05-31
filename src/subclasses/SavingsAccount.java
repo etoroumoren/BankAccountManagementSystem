f13c450b91336554cb75f93b57a981307010d7fd
Etoro Umoren <torohkiss@yahoo.com>
2026-05-31 12:07:45 +0100

    feat: SavingsAccount

---
+++ src/subclasses/SavingsAccount.java
@@ -0,0 +1,46 @@
+package subclasses;
+
+import base.BankAccount;
+
+public class SavingsAccount extends BankAccount {
+
+    private double interestRate;
+    private double withdrawalLimits;
+
+    public SavingsAccount(int accountNumber, double balance, String owner, double interestRate, double withdrawalLimits) {
+        super(accountNumber, balance, owner);
+        this.interestRate = interestRate;
+        this.withdrawalLimits = withdrawalLimits;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public double getWithdrawalLimits() {
+        return withdrawalLimits;
+    }
+
+    public void setWithdrawalLimits(double withdrawalLimits) {
+        this.withdrawalLimits = withdrawalLimits;
+    }
+
+    @Override
+    public String getAccountType() {
+        return "Savings account";
+    }
+
+    @Override
+    public void withdraw(int amount) {
+        if(amount < 0) return;
+        if(getBalance() - amount < 150) {
+            return;
+        }
+
+        super.withdraw(amount);
+    }
+}
