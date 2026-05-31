ebf1b7f2baee5aad9522cd6bcba9a6caef915549
Etoro Umoren <torohkiss@yahoo.com>
2026-05-31 12:39:25 +0100

    feat: CurrentAccountAccount

---
+++ src/subclasses/CurrentAccount.java
@@ -0,0 +1,35 @@
+package subclasses;
+
+import base.BankAccount;
+
+public class CurrentAccount extends BankAccount {
+
+    private double overdraftLimit;
+
+    public CurrentAccount(int accountNumber, double balance, String owner, double overdraftLimit) {
+        super(accountNumber, balance, owner);
+        this.overdraftLimit = overdraftLimit;
+    }
+
+    public String getAccountType() {
+        return "Current account";
+    }
+
+    public double getOverdraftLimit() {
+        return overdraftLimit;
+    }
+
+    public void setOverdraftLimit(double overdraftLimit) {
+        this.overdraftLimit = overdraftLimit;
+    }
+
+    @Override
+    public void withdraw(int amount){
+        if (amount <= 0) return;
+
+        if(getBalance() - amount < -overdraftLimit) {
+            return;
+        }
+        super.withdraw(amount);
+    }
+}
